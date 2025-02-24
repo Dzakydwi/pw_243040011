9677cee7b2d7be27e158a9fba6ba4d4b031a790f
Dzakydwi <jekidzak15@gmail.com>
2025-02-24 15:29:57 +0700

    tugas 2 php selesai

---
+++ tugas/tugas 2/2a.php
@@ -0,0 +1,16 @@
+<?php
+    $namaDepan = "Dwi";
+    $namaBelakang = "Dzaky";
+    
+    for ($i = 1; $i <= 100; $i++) {
+        if ($i % 3 == 0 && $i % 5 == 0) {
+            echo $namaDepan . " " . $namaBelakang . "<br>";
+        } elseif ($i % 3 == 0) {
+            echo $namaDepan . "<br>";
+        } elseif ($i % 5 == 0) {
+            echo $namaBelakang . "<br>";
+        } else {
+            echo $i . "<br>";
+        }
+    }
+?>
