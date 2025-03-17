9a3867164af3a51f745a5f634f8e4b7af2b73955
Dzakydwi <jekidzak15@gmail.com>
2025-03-17 20:44:45 +0700

    selesai mengerjakan tugas 4

--- kuliah/pertemuan2/latihan3.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $nama_depan = "Dwi";
-    $nama_belakang = "Dzaky Wibowo";
-
-    for ($i = 100; $1 >= 1; ) {
-        if ()
-    }
-?>
---
+++ tugas/tugas 4/4a.php
@@ -0,0 +1,4 @@
+<?php
+$kata = ["depanmu ", "diciptakan oleh apa yang kau ", "kerjakan hari ini, ", "bukan ",  "Masa ", "besok "];
+
+echo $kata[4] . $kata[0] . $kata[1] . $kata[2] . $kata[3] . $kata[5];
